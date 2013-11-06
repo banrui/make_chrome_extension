844a972045a3145028057af336c1155ceeb4e900
banrui <bando.rui@gmail.com>
2013-11-06 21:03:33 +0900

    modify main source

--- lib/make_chrome_extension.rb
+++ lib/make_chrome_extension.rb
@@ -2,6 +2,38 @@ require "make_chrome_extension/version"
 
 module MakeChromeExtension
   def self.new_template(app_name)
+      # make main dir
       Dir::mkdir("./#{app_name}")
+      
+      # make manifest file
+      make_manifest(app_name)
+  end
+
+  def make_manifest(app_name)
+      contents <<- EOF
+{
+	"manifest_version": 2,
+	"name": "#{app_name}",
+//TODO	"description": "Delete the cookie and history of your web browser for your privacy",
+	"version": "1",
+	"browser_action": {
+//TODO			  "default_icon": "images/icon.png"
+			  },
+//	"icons": {
+//		 "16": "images/16.png",
+//		 "48": "images/48.png",
+//		 "128": "images/128.png"
+//		 },
+//TODO	"permissions": ["http://*/*", "https://*/*"],
+	"background": {
+		      "scripts": ["background.js"]
+		      },
+//	"options_page": "options.html",
+//	"web_accessible_resources": ["images/48.png"]
+}
+      EOF
+      manifest_file = File.open("manifest.json", "w")
+      manifest_file.write(contents)
+      manifest_file.close       
   end
 end
